b942977960f0f101997a095db4bed316bd32db56
Kuvernoori <sanjarberkamalov@gmail.com>
2025-11-24 01:45:21 +0500

    UserDtoUpd

--- BilgenlyD.API/Models/DTOs/User/CreateUserDto.cs
+++ BilgenlyD.API/Models/DTOs/User/CreateUserDto.cs
@@ -1,8 +1,8 @@
 ﻿namespace BilgenlyD.Models.DTOs;
 
-public class UserDto
+public class CreateUserDto
 {
-    public int Id { get; set; }
     public string Username { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
 }
---
+++ BilgenlyD.API/Models/DTOs/User/UpdateUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BilgenlyD.Models.DTOs;
+
+public class UpdateUserDto
+{
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
